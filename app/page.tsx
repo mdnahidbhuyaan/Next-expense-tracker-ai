1f7329012ed47b4e5cc064392a2670e66cb73a17
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-08-05 18:52:21 +0600

    five components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+
+
+export default async function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const user = await currentUser();
+if(!user){
+  return <Guest/>
+}
+
+
+
   return (
 <div>
   <h2 className="text-red-500">Hello HomePage</h2>
